1eda04affd05c5520902476dd5a1c699b0d848d8
chengxi <chengxi1014@live.com>
2026-01-20 11:10:11 +0800

    下载脚本

--- scripts/download_ltx2.sh
+++ scripts/download_ltx2.sh
@@ -3,8 +3,9 @@
 cd /workspace/
 
 pip install -U huggingface_hub
-hf download Lightricks/LTX-2
+hf download Lightricks/LTX-2 --include "transformer/*" "text_encoder/*" "tokenizer/*" "vae/*" "audio_vae/*" "connectors/*" "vocoder/*" "*.json"
 
 git clone https://github.com/xiaoxidashen/ai-toolkit.git
 cd ai-toolkit
 pip install -r requirements.txt
+
